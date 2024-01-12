a18451cb6dd4a3735ea5a50fabdecb587f2aadb6
nicolas.romanzin <nicolas.romanzin@thalesgroup.com>
2024-01-12 15:05:07 +0100

    2nd TDD iteration - should be 2 if input is 2

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -4,4 +4,8 @@ describe('Kata', () => {
   test('should get 1 if input is 1', () => {
     expect(fizzbuzz(1)).toStrictEqual('1');
   });
+
+  test('should get 2 if input is 2', () => {
+    expect(fizzbuzz(2)).toStrictEqual('2');
+  });
 });
--- src/kata.ts
+++ src/kata.ts
@@ -1,3 +1,3 @@
 export function fizzbuzz(input: number): string {
-  return '1';
+  return input.toString();
 }
