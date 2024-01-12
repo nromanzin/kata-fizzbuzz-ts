c847921fcf679eba33c572b59ca6f42bffcc45c6
nicolas.romanzin <nicolas.romanzin@thalesgroup.com>
2024-01-12 15:11:44 +0100

    4th TDD iteration - parameterized tests and work up to 5

--- src/kata.spec.ts
+++ src/kata.spec.ts
@@ -1,15 +1,13 @@
 import { fizzbuzz } from './kata';
 
 describe('Kata', () => {
-  test('should get 1 if input is 1', () => {
-    expect(fizzbuzz(1)).toStrictEqual('1');
-  });
-
-  test('should get 2 if input is 2', () => {
-    expect(fizzbuzz(2)).toStrictEqual('2');
-  });
-
-  test('should get Fizz if input is 3', () => {
-    expect(fizzbuzz(3)).toStrictEqual('Fizz');
+  test.each([
+    [1, '1'],
+    [2, '2'],
+    [3, 'Fizz'],
+    [4, '4'],
+    [5, 'Buzz'],
+  ])('should convert %p to %p', (input, expected) => {
+    expect(fizzbuzz(input)).toStrictEqual(expected);
   });
 });
--- src/kata.ts
+++ src/kata.ts
@@ -2,5 +2,8 @@ export function fizzbuzz(input: number): string {
   if (input === 3) {
     return 'Fizz';
   }
+  if (input == 5) {
+    return 'Buzz';
+  }
   return input.toString();
 }
